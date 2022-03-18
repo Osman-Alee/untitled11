abdbef2f20926c4ed63da723c753bada49d530e5
faizan6590 <faizanyousaf6590@gmail.com>
2022-03-18 17:18:36 +0500

    faizan's second commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import * as React from 'react';
 import SignUp from "./SignUp";
 import SignIn from "./SignIn";
 import ResponsiveAppBar from "./ResponsiveAppBar";
-
+import CustomizedTables from "./CustomizedTables";
 function App() {
   return (
       <>
           <ResponsiveAppBar/>
           <SignUp/>
           <SignIn/>
+          <CustomizedTables/>
       </>
   );
 }
