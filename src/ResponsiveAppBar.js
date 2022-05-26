7944ac5ce3bed7bcdc0d76de1da580bf39b3e66a
Osman-Alee <73214576+Osman-Alee@users.noreply.github.com>
2022-05-26 18:57:13 +0500

    Update ResponsiveAppBar.js

--- src/ResponsiveAppBar.js
+++ src/ResponsiveAppBar.js
@@ -11,7 +11,7 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import SignIn from "./SignIn";
+// import SignIn from "./SignIn";
 
 const pages = ['Products', 'Pricing', 'Blog'];
 const settings = ['SignUp', 'SignIn', 'Dashboard', 'Logout'];
